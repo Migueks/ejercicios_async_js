07ce02c5aab32d776f96aa1c22826d0cd1e51582
Migueks <lhlmiguel@gmail.com>
2024-10-19 14:16:11 +0200

    ejercicio 1 finalizado

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -3,11 +3,18 @@ const request = async () => {
   const response = await res.json();
 
   const selectLabel = document.getElementById("character-list");
+  const imgCharacter = document.querySelector(".character-image");
 
   response.forEach((character) => {
     const optionLabel = document.createElement("option");
     optionLabel.textContent = character.firstName;
+    optionLabel.value = character.imageUrl;
     selectLabel.appendChild(optionLabel);
+  });
+
+  selectLabel.addEventListener("change", (event) => {
+    const selectedImageUrl = event.target.value;
+    imgCharacter.src = selectedImageUrl;
   });
 };
 
