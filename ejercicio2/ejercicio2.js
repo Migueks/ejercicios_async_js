acf3b193bb703c11022a669e7aa16a65a48d5e71
Migueks <lhlmiguel@gmail.com>
2024-10-20 22:17:06 +0200

    ejercicio 2 finalizado

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,9 +1,32 @@
 const pokeRequest = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const data = await res.json();
+
+    const randomPokemon = getRandomPokemon(data.results);
+
+    const imgHtmlArray = document.getElementsByClassName("random-image");
+    const imgHtml = imgHtmlArray[0];
+
+    const pokemonImg = await getImagePokemon(randomPokemon.url);
+    imgHtml.src = pokemonImg;
+
+    console.log(imgHtml);
+    console.log(pokemonImg);
+  } catch (error) {
+    console.log("Se ha producido un error:", error);
+  }
 };
 
-//https://www.youtube.com/watch?v=EmxvMPcIy5c&ab_channel=CarpiCoder
+const getRandomPokemon = (pokemonArray) => {
+  const randomNumber = Math.floor(Math.random() * pokemonArray.length);
+  return pokemonArray[randomNumber];
+};
+
+const getImagePokemon = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.sprites.front_default;
+};
 
 pokeRequest();
